2947ba5b1d6a3aa63d8fb217f3ad182acf401ac9
dreamz0r <110413248+dreamz0r@users.noreply.github.com>
2025-04-29 15:29:39 +0300

    Add files via upload

---
+++ anims/anm/player/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches
+{
+	class DZ_Anims_Anm_Player
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={};
+	};
+};
---
+++ anims/cfg/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches
+{
+	class DZ_Anims_Cfg
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={};
+	};
+};
